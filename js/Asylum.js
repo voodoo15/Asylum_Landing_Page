d73b629a8ffcd147e4820ce6a2732823bd8f8944
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2017-02-19 09:03:01 -0500

    Fixed the nav-default so it shows up on mobile at the start.

--- js/Asylum.js
+++ js/Asylum.js
@@ -1,26 +1,35 @@
 // SMOOTH SCROLL
 $('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
     }
-  }
 });
 
 // NAV CLASS CHANGE
+
+if (screen.width < 800) {
+    $("nav").addClass("navbar-default");
+    $("nav").removeClass("navbar-transparent");
+} else {
+    $("nav").addClass("navbar-transparent");
+    $("nav").removeClass("navbar-default");
+}
+
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if(scroll >= 500) {
+    if (scroll >= 800) {
         $("nav").addClass("navbar-default");
         $("nav").removeClass("navbar-transparent");
     } else {
-      $("nav").addClass("navbar-transparent").fadeIn("slow");
+        $("nav").addClass("navbar-transparent");
         $("nav").removeClass("navbar-default");
     }
 });
