ee7fe50fec9046bfdc4282126b9bd7778c2bd66c
Arnold Redoblado <VOODOO15@VOODOO15-MBP.local>
2017-03-06 06:51:41 -0500

    Added JS to hide/show class options for Paypal.  Added images provided for a few of the digital media courses.

--- js/Asylum.js
+++ js/Asylum.js
@@ -12,23 +12,147 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
 });
 
-$(document).ready( () => {
+$(document).ready(() => {
 
-  // NAV CLASS CHANGE
-  if (screen.width <= 768) {
-    $("nav").addClass("navbar-default");
-    $("nav").removeClass("navbar-transparent");
-  } else {
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-
-      if (scroll > 600) {
+    // NAV CLASS CHANGE
+    if (screen.width <= 768) {
         $("nav").addClass("navbar-default");
         $("nav").removeClass("navbar-transparent");
-      } else {
-        $("nav").addClass("navbar-transparent");
-        $("nav").removeClass("navbar-default");
-      }
+    } else {
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+
+            if (scroll > 600) {
+                $("nav").addClass("navbar-default");
+                $("nav").removeClass("navbar-transparent");
+            } else {
+                $("nav").addClass("navbar-transparent");
+                $("nav").removeClass("navbar-default");
+            }
+        });
+    }
+
+    //HIDE PAYPAL OPTIONS
+    $("#portfolioArtPaymentForm input[name='on7']").parent('td').hide();
+    $("#portfolioArtPaymentForm select[name='os7']").hide();
+    $("#portfolioArtPaymentForm input[name='on3']").parent('td').hide();
+    $("#portfolioArtPaymentForm select[name='os3']").hide();
+    $("#portfolioArtPaymentForm input[name='on4']").parent('td').hide();
+    $("#portfolioArtPaymentForm select[name='os4']").hide();
+
+    $("#AnimationPaymentForm input[name='on7']").parent('td').hide();
+    $("#AnimationPaymentForm select[name='os7']").hide();
+    $("#AnimationPaymentForm input[name='on3']").parent('td').hide();
+    $("#AnimationPaymentForm select[name='os3']").hide();
+    $("#AnimationPaymentForm input[name='on4']").parent('td').hide();
+    $("#AnimationPaymentForm select[name='os4']").hide();
+
+    $("#lifeDrawingPaymentForm input[name='on7']").parent('td').hide();
+    $("#lifeDrawingPaymentForm select[name='os7']").hide();
+    $("#lifeDrawingPaymentForm input[name='on3']").parent('td').hide();
+    $("#lifeDrawingPaymentForm select[name='os3']").hide();
+    $("#lifeDrawingPaymentForm input[name='on4']").parent('td').hide();
+    $("#lifeDrawingPaymentForm select[name='os4']").hide();
+
+    //SHOW/HIDE PAYPAL OPTIONS ON CHANGE
+    $("#portfolioArtPaymentForm select[name='os0']").change(function() {
+
+        if ($("#portfolioArtPaymentForm select[name='os0'] option:selected").index() === 0) {
+            $("#portfolioArtPaymentForm input[name='on7']").parent('td').hide();
+            $("#portfolioArtPaymentForm select[name='os7']").hide();
+            $("#portfolioArtPaymentForm input[name='on3']").parent('td').hide();
+            $("#portfolioArtPaymentForm select[name='os3']").hide();
+            $("#portfolioArtPaymentForm input[name='on4']").parent('td').hide();
+            $("#portfolioArtPaymentForm select[name='os4']").hide();
+        } else if ($("#portfolioArtPaymentForm select[name='os0'] option:selected").index() === 1) {
+            $("#portfolioArtPaymentForm input[name='on7']").parent('td').hide();
+            $("#portfolioArtPaymentForm select[name='os7']").hide();
+            $("#portfolioArtPaymentForm input[name='on3']").parent('td').show();
+            $("#portfolioArtPaymentForm select[name='os3']").show();
+            $("#portfolioArtPaymentForm input[name='on4']").parent('td').hide();
+            $("#portfolioArtPaymentForm select[name='os4']").hide();
+        } else if ($("#portfolioArtPaymentForm select[name='os0'] option:selected").index() === 2) {
+            $("#portfolioArtPaymentForm input[name='on7']").parent('td').hide();
+            $("#portfolioArtPaymentForm select[name='os7']").hide();
+            $("#portfolioArtPaymentForm input[name='on3']").parent('td').show();
+            $("#portfolioArtPaymentForm select[name='os3']").show();
+            $("#portfolioArtPaymentForm input[name='on4']").parent('td').show();
+            $("#portfolioArtPaymentForm select[name='os4']").show();
+        } else if ($("#portfolioArtPaymentForm select[name='os0'] option:selected").index() === 3) {
+            $("#portfolioArtPaymentForm input[name='on7']").parent('td').show();
+            $("#portfolioArtPaymentForm select[name='os7']").show();
+            $("#portfolioArtPaymentForm input[name='on3']").parent('td').show();
+            $("#portfolioArtPaymentForm select[name='os3']").show();
+            $("#portfolioArtPaymentForm input[name='on4']").parent('td').show();
+            $("#portfolioArtPaymentForm select[name='os4']").show();
+        }
     });
-  }
+
+    $("#AnimationPaymentForm select[name='os0']").change(function() {
+
+        if ($("#AnimationPaymentForm select[name='os0'] option:selected").index() === 0) {
+            $("#AnimationPaymentForm input[name='on7']").parent('td').hide();
+            $("#AnimationPaymentForm select[name='os7']").hide();
+            $("#AnimationPaymentForm input[name='on3']").parent('td').hide();
+            $("#AnimationPaymentForm select[name='os3']").hide();
+            $("#AnimationPaymentForm input[name='on4']").parent('td').hide();
+            $("#AnimationPaymentForm select[name='os4']").hide();
+        } else if ($("#AnimationPaymentForm select[name='os0'] option:selected").index() === 1) {
+            $("#AnimationPaymentForm input[name='on7']").parent('td').hide();
+            $("#AnimationPaymentForm select[name='os7']").hide();
+            $("#AnimationPaymentForm input[name='on3']").parent('td').show();
+            $("#AnimationPaymentForm select[name='os3']").show();
+            $("#AnimationPaymentForm input[name='on4']").parent('td').hide();
+            $("#AnimationPaymentForm select[name='os4']").hide();
+        } else if ($("#AnimationPaymentForm select[name='os0'] option:selected").index() === 2) {
+            $("#AnimationPaymentForm input[name='on7']").parent('td').hide();
+            $("#AnimationPaymentForm select[name='os7']").hide();
+            $("#AnimationPaymentForm input[name='on3']").parent('td').show();
+            $("#AnimationPaymentForm select[name='os3']").show();
+            $("#AnimationPaymentForm input[name='on4']").parent('td').show();
+            $("#AnimationPaymentForm select[name='os4']").show();
+        } else if ($("#AnimationPaymentForm select[name='os0'] option:selected").index() === 3) {
+            $("#AnimationPaymentForm input[name='on7']").parent('td').show();
+            $("#AnimationPaymentForm select[name='os7']").show();
+            $("#AnimationPaymentForm input[name='on3']").parent('td').show();
+            $("#AnimationPaymentForm select[name='os3']").show();
+            $("#AnimationPaymentForm input[name='on4']").parent('td').show();
+            $("#AnimationPaymentForm select[name='os4']").show();
+        }
+    });
+
+    $("#lifeDrawingPaymentForm select[name='os0']").change(function() {
+
+        if ($("#lifeDrawingPaymentForm select[name='os0'] option:selected").index() === 0) {
+            $("#lifeDrawingPaymentForm  input[name='on7']").parent('td').hide();
+            $("#lifeDrawingPaymentForm select[name='os7']").hide();
+            $("#lifeDrawingPaymentForm  input[name='on3']").parent('td').hide();
+            $("#lifeDrawingPaymentForm select[name='os3']").hide();
+            $("#lifeDrawingPaymentForm  input[name='on4']").parent('td').hide();
+            $("#lifeDrawingPaymentForm select[name='os4']").hide();
+        } else if ($("#lifeDrawingPaymentForm select[name='os0'] option:selected").index() === 1) {
+            $("#lifeDrawingPaymentForm  input[name='on7']").parent('td').hide();
+            $("#lifeDrawingPaymentForm select[name='os7']").hide();
+            $("#lifeDrawingPaymentForm  input[name='on3']").parent('td').show();
+            $("#lifeDrawingPaymentForm select[name='os3']").show();
+            $("#lifeDrawingPaymentForm  input[name='on4']").parent('td').hide();
+            $("#lifeDrawingPaymentForm select[name='os4']").hide();
+        } else if ($("#lifeDrawingPaymentForm select[name='os0'] option:selected").index() === 2) {
+            $("#lifeDrawingPaymentForm  input[name='on7']").parent('td').hide();
+            $("#lifeDrawingPaymentForm select[name='os7']").hide();
+            $("#lifeDrawingPaymentForm  input[name='on3']").parent('td').show();
+            $("#lifeDrawingPaymentForm select[name='os3']").show();
+            $("#lifeDrawingPaymentForm  input[name='on4']").parent('td').show();
+            $("#lifeDrawingPaymentForm select[name='os4']").show();
+        } else if ($("#lifeDrawingPaymentForm select[name='os0'] option:selected").index() === 3) {
+            $("#lifeDrawingPaymentForm  input[name='on7']").parent('td').show();
+            $("#lifeDrawingPaymentForm select[name='os7']").show();
+            $("#lifeDrawingPaymentForm  input[name='on3']").parent('td').show();
+            $("#lifeDrawingPaymentForm select[name='os3']").show();
+            $("#lifeDrawingPaymentForm  input[name='on4']").parent('td').show();
+            $("#lifeDrawingPaymentForm select[name='os4']").show();
+        }
+    });
+
+
 }); // Close $(document).ready()
